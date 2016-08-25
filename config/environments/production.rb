f4b55eff901d892b317a75a079e1fb4bf6950105
Glacieren <git@glacieren.com>
2016-08-25 22:42:30 +0000

    Added production.rb lines on line 80

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # These are here in order to let images deploy to heroku
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
