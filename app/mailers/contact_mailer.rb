0ea649680b579ba590bf9e921a5e10b439af1ffd
Glacieren <git@glacieren.com>
2016-08-31 23:34:12 +0000

    Added email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'dallaslund@gmail.com'
+    default to: 'glacierendomains@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
